f9357a612a745ccefc39012ecf59c7c152c948d3
Tosin <tosin.akinosho@gmail.com>
2020-10-04 22:47:27 -0400

    adding ansible runner service

--- lib/configure_ansible_runner.sh
+++ lib/configure_ansible_runner.sh
@@ -4,12 +4,12 @@
 function install_ansible_runner_service(){
     if [ ! -d /usr/share/ansible-runner-service ];
     then
-        mkdir -p p/home/${USER}/qubinode-installer/env
+        mkdir -p /home/"${USER}"/qubinode-installer/env
         git clone https://github.com/ansible/ansible-runner-service.git
         sudo  mkdir -p /etc/ansible-runner-service
         sudo mkdir -p /usr/share/ansible-runner-service/{artifacts,env,project,inventory,client_cert}
         sudo mv ansible-runner-service /usr/share/ansible-runner-service/
-        cd /opt/ansible-runner-service 
+        cd /opt/ansible-runner-service || exit 
         sudo cp ansible_runner_service /usr/bin/
         #sudo  python3 setup.py install --record installed_files --single-version-externally-managed
         #sudo ansible_runner_service
@@ -23,12 +23,13 @@ function configure_ansible_runner_systemd(){
       sudo systemctl enable ansible-runner-service.service
       sudo systemctl start ansible-runner-service.service
       sudo systemctl status ansible-runner-service.service
+    fi
 }
 
 # Change valut key path in ansible.cfg file
 function update_ansible_cfg(){
-    if [ -f /home/${USER}/qubinode-installer/ansible.cfg ];
+    if [ -f /home/"${USER}"/qubinode-installer/ansible.cfg ];
     then 
-      sed -i 's/vault_password_file  = ~\/.vaultkey/vault_password_file  = \/home\/'${USER}'\/.vaultkey/g'  /home/${USER}/qubinode-installer/ansible.cfg
+      sed -i 's/vault_password_file  = ~\/.vaultkey/vault_password_file  = \/home\/'"${USER}"'\/.vaultkey/g'  /home/"${USER}"/qubinode-installer/ansible.cfg
     fi 
 }
