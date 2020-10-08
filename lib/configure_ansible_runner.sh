af40483835506f5b04a3fd5eca9acfccfd6b84f7
Tosin <tosin.akinosho@gmail.com>
2020-10-08 12:16:42 -0400

    updating ansible runner service code

--- lib/configure_ansible_runner.sh
+++ lib/configure_ansible_runner.sh
@@ -29,7 +29,7 @@ version: 1
 # playbooks_root_dir
 # location of the playbooks that the service will start
 # playbooks_root_dir: './samples'
-playbooks_root_dir: '/home/${USER}/qubinode-installer/playbooks'
+playbooks_root_dir: '/home/${USER}/qubinode-installer/'
 
 # port
 # tcp port for the service to listen to
